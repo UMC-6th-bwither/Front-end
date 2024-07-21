9250dceb3f62b4a44f5338450ba2d718a3cd8fbb
jisooooooooooo <dlawltn0708@naver.com>
2024-07-21 10:58:33 +0900

    ✨ Feat: 채팅 브리더 정보 컴포넌트 생성

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,28 @@
+// import { useState } from 'react';
+// import MenuSelect from './components/MenuSelect';
+import ChatBreederInfo from './components/ChatBreederInfo';
+
 function App() {
-  return <>App</>;
+  // const [activieMenu, setActiveMenu] = useState('브리더 정보');
+
+  // const menu = [
+  //   '브리더 정보',
+  //   '켄넬 정보',
+  //   '관리 중인 강아지',
+  //   '후기 (22)',
+  //   '질문/답변',
+  //   '커뮤니티',
+  // ];
+  return (
+    <>
+      {/* <MenuSelect
+        menus={menu}
+        activeMenu={activieMenu}
+        setActiveMenu={setActiveMenu}
+      />{' '} */}
+      <ChatBreederInfo />
+    </>
+  );
 }
 
 export default App;
