cce55f399ab98c3632a588d6b48a6905d5a8cdb9
jisooooooooooo <dlawltn0708@naver.com>
2024-07-22 22:55:48 +0900

    ♻️ Refactor: 폴더 구조 수정

--- src/components/MenuSelect/MenuSelect.jsx
+++ src/components/MenuSelect/MenuSelect.jsx
@@ -1,5 +1,5 @@
 import PropTypes from 'prop-types';
-import * as M from '../MenuSelect/MenuSelect.style';
+import * as M from './MenuSelect.style';
 
 function MenuSelect({ menus, activeMenu, setActiveMenu }) {
   return (
