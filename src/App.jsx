aa80045edf20e91e9a5c1026700cd5ea289f89cc
kwdahun <kwdahun@naver.com>
2024-07-24 22:40:56 +0900

    ✨ Feat: URL Routing 설정

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,18 @@
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import AnimalList from './pages/AnimalList/AnimalList';
+import NotFound from './pages/NotFound/NotFound';
+import ButtonBadgeSample from './pages/ButtonBadgeSample/ButtonBadgeSample';
 
 function App() {
-  // return <>App</>;
-  return <AnimalList />;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<AnimalList />} />
+        <Route path="/ButtonBadgeSample" element={<ButtonBadgeSample />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
