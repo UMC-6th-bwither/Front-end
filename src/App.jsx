417bb15281fefa532a87cad27074083340913548
kayubin <kayubin27@gmail.com>
2024-07-27 00:41:58 +0900

    ✨ Feat: 라우팅 설정

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,33 @@
+/* eslint-disable import/no-unresolved */
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+
+import Header from './components/header/Header';
 import AnimalList from './pages/AnimalList/AnimalList';
+import BreederList from './pages/BreederList/BreederList';
 import NotFound from './pages/NotFound/NotFound';
 import ButtonBadgeSample from './pages/ButtonBadgeSample/ButtonBadgeSample';
+import Animal from './pages/MyReview/Animal/Animal';
+import Breeder from './pages/MyReview/Breeder/Breeder';
+import DropBox from './components/dropBox/DropBox';
+import Footer from './components/Footer/Footer';
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<AnimalList />} />
-        <Route path="/ButtonBadgeSample" element={<ButtonBadgeSample />} />
-        <Route path="*" element={<NotFound />} />
-      </Routes>
-    </Router>
+    <>
+      <Header />
+      <Router>
+        <Routes>
+          <Route path="/DropBox" element={<DropBox />} />
+          <Route path="/Animal" element={<Animal />} />
+          <Route path="/Breeder" element={<Breeder />} />
+          <Route path="/" element={<AnimalList />} />
+          <Route path="/BreederList" element={<BreederList />} />
+          <Route path="/ButtonBadgeSample" element={<ButtonBadgeSample />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </Router>
+      <Footer />
+    </>
   );
 }
 
