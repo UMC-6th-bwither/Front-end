d84a269b47f3469584fbd415a3f2795c1b0fa38a
kayubin <kayubin27@gmail.com>
2024-08-08 19:01:49 +0900

    🎨 구조 변경

--- src/pages/MyReview/Animal/Animal.jsx
+++ src/pages/MyReview/Animal/Animal.jsx
@@ -1,13 +1,12 @@
 /* eslint-disable react/no-array-index-key */
 import { useState, useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { toggleBookmark } from '../../../redux/bookmarkSlice';
 import * as A from './Animal.style';
 import bookmark from '../../../../public/img/bookmark.svg';
 import footprint from '../../../../public/img/footprint.svg';
-import { animalBreeds } from '../../selectData';
+import { animalBreeds, 전체DogCard } from '../../selectData';
 import DogCard from '../../../components/DogCard/DogCard';
 import DropBox from '../../../components/DropBoxes/DropBox';
+import Pagination from '../../../components/Pagination/Pagination';
 
 function Animal() {
   const [selectedAnimal, setSelectedAnimal] = useState('');
@@ -15,13 +14,7 @@ function Animal() {
   const [selectedGender, setSelectedGender] = useState('');
   const [selectedBreed, setSelectedBreed] = useState('');
   const [isReserved, setIsReserved] = useState(false);
-
-  const dispatch = useDispatch();
-  const bookmarks = useSelector((state) => state.bookmarks.card);
-
-  useEffect(() => {
-    console.log('Animal Page Bookmarks:', bookmarks);
-  }, [bookmarks]); // 상태가 변경될 때마다 콘솔 출력
+  const [currentPage, setCurrentPage] = useState(1);
 
   useEffect(() => {
     setBreeds(animalBreeds[selectedAnimal] || []);
@@ -45,37 +38,31 @@ function Animal() {
   };
 
   // 선택된 조건에 따른 DogCard데이터 필터링
-  const bookmaredPet = bookmarks.filter((pet) => {
+  const bookmaredPet = 전체DogCard.filter((pet) => {
     return (
       (selectedGender === '' || pet.gender === selectedGender) &&
       (selectedBreed === '' || pet.breed === selectedBreed) &&
-      (!isReserved || pet.waitlistCount === 0) &&
-      pet.isBookmarked
-      // bookmarks[pet.name]
+      (!isReserved || pet.waitlistCount === 0)
     );
   });
 
   return (
     <A.Border>
       <A.SideHeader>
-        {/* <div className="community">
-        <A.MenuTitle>커뮤니티</A.MenuTitle>
-        <A.MenuSubtitleContainer>
-          <A.MenuSubtitleActive to="/community/question">
-            질문/고민
-          </A.MenuSubtitleActive>
-          <A.MenuSubtitleInActive to="/community/information">
-            정보 공유
-          </A.MenuSubtitleInActive>
-          <A.MenuSubtitleInActive to="/community/daily">
-            일상 이야기
-          </A.MenuSubtitleInActive>
-          <A.MenuSubtitleInActive to="/community/breederinformation">
-            브리더의 꿀정보
-          </A.MenuSubtitleInActive>
-          <A.MenuSubtitleInActive>브위더 후기</A.MenuSubtitleInActive>
-        </A.MenuSubtitleContainer>
-      </div> */}
+        <div className="community">
+          <A.MenuTitle>커뮤니티</A.MenuTitle>
+          <A.MenuSubtitleContainer>
+            <A.MenuSubtitleInActive to="/myreview/post">
+              내가 쓴 글
+            </A.MenuSubtitleInActive>
+            <A.MenuSubtitleInActive to="/myreview/comment">
+              내가 단 글
+            </A.MenuSubtitleInActive>
+            <A.MenuSubtitleInActive to="/myreview/review">
+              나의 후기
+            </A.MenuSubtitleInActive>
+          </A.MenuSubtitleContainer>
+        </div>
         <div className="scrap">
           <A.MenuTitle>스크랩</A.MenuTitle>
           <A.MenuSubtitleContainer>
@@ -85,9 +72,9 @@ function Animal() {
             <A.MenuSubtitleInActive to="/myreview/breeder">
               저장한 브리더
             </A.MenuSubtitleInActive>
-            {/* <A.MenuSubtitleActive to="/myreview/save">
-            저장한 글
-          </A.MenuSubtitleActive> */}
+            <A.MenuSubtitleActive to="/myreview/save">
+              저장한 글
+            </A.MenuSubtitleActive>
           </A.MenuSubtitleContainer>
         </div>
       </A.SideHeader>
@@ -138,26 +125,34 @@ function Animal() {
             </A.Left>
           </A.SelectContainer>
           <A.CardsContainer>
-            {bookmaredPet.map(
-              (
-                pet,
-                index, // 필터링된 카드 나열
-              ) => (
-                <DogCard
-                  key={index}
-                  photo={pet.photo}
-                  location={pet.location}
-                  name={pet.name}
-                  breed={pet.breed}
-                  birthDate={pet.birthDate}
-                  gender={pet.gender}
-                  breederName={pet.breederName}
-                  waitlistCount={pet.waitlistCount}
-                  isBookmarked={pet.isBookmarked}
-                  setIsBookmarked={() => dispatch(toggleBookmark(pet.name))}
-                />
-              ),
-            )}
+            <div className="dogCard">
+              {bookmaredPet.map(
+                (
+                  pet,
+                  index, // 필터링된 카드 나열
+                ) => (
+                  <DogCard
+                    key={index}
+                    photo={pet.photo}
+                    location={pet.location}
+                    name={pet.name}
+                    breed={pet.breed}
+                    birthDate={pet.birthDate}
+                    gender={pet.gender}
+                    breederName={pet.breederName}
+                    waitlistCount={pet.waitlistCount}
+                    isBookmarked={pet.isBookmarked}
+                    setIsBookmarked={() => {}}
+                  />
+                ),
+              )}
+            </div>
+            <Pagination
+              totalItems={bookmaredPet.length}
+              itemsPerPage={20}
+              currentPage={currentPage}
+              setCurrentPage={setCurrentPage}
+            />
           </A.CardsContainer>
         </A.BottomContainer>
       </A.MainContainer>
--- src/pages/MyReview/Breeder/Breeder.jsx
+++ src/pages/MyReview/Breeder/Breeder.jsx
@@ -2,16 +2,15 @@
 import { useState, useEffect } from 'react';
 import * as A from './Breeder.style';
 import bookmark from '../../../../public/img/bookmark.svg';
-import { animalBreeds } from '../../selectData';
+import { animalBreeds, 전체BreederCard } from '../../selectData';
 import BreederCard from '../../../components/BreederCard/BreederCard';
 import DropBox from '../../../components/DropBoxes/DropBox';
+import Pagination from '../../../components/Pagination/Pagination';
 
 function Breeder() {
-  //  ** Breeder Card API호출 필요 **
-
   const [selectedAnimal, setSelectedAnimal] = useState('');
   const [breeds, setBreeds] = useState([]);
-  // const bookmarkedBreeder = 전체BreederCard.filter(breeder => breeder.isBookmarked); 북마크된 브리더 필터링
+  const [currentPage, setCurrentPage] = useState(1);
 
   useEffect(() => {
     setBreeds(animalBreeds[selectedAnimal] || []);
@@ -25,36 +24,32 @@ function Breeder() {
   return (
     <A.Border>
       <A.SideHeader>
-        {/* <div className="community">
-        <A.MenuTitle>커뮤니티</A.MenuTitle>
-        <A.MenuSubtitleContainer>
-          <A.MenuSubtitleActive to="/community/question">
-            질문/고민
-          </A.MenuSubtitleActive>
-          <A.MenuSubtitleInActive to="/community/information">
-            정보 공유
-          </A.MenuSubtitleInActive>
-          <A.MenuSubtitleInActive to="/community/daily">
-            일상 이야기
-          </A.MenuSubtitleInActive>
-          <A.MenuSubtitleInActive to="/community/breederinformation">
-            브리더의 꿀정보
-          </A.MenuSubtitleInActive>
-          <A.MenuSubtitleInActive>브위더 후기</A.MenuSubtitleInActive>
-        </A.MenuSubtitleContainer>
-      </div> */}
+        <div className="community">
+          <A.MenuTitle>커뮤니티</A.MenuTitle>
+          <A.MenuSubtitleContainer>
+            <A.MenuSubtitleInActive to="/myreview/post">
+              내가 쓴 글
+            </A.MenuSubtitleInActive>
+            <A.MenuSubtitleInActive to="/myreview/comment">
+              내가 단 글
+            </A.MenuSubtitleInActive>
+            <A.MenuSubtitleInActive to="/myreview/review">
+              나의 후기
+            </A.MenuSubtitleInActive>
+          </A.MenuSubtitleContainer>
+        </div>
         <div className="scrap">
           <A.MenuTitle>스크랩</A.MenuTitle>
           <A.MenuSubtitleContainer>
             <A.MenuSubtitleInActive to="/myreview/animal">
               저장한 동물
             </A.MenuSubtitleInActive>
-            <A.MenuSubtitleInActive to="/myreview/breeder">
+            <A.MenuSubtitleActive to="/myreview/breeder">
               저장한 브리더
+            </A.MenuSubtitleActive>
+            <A.MenuSubtitleInActive to="/myreview/save">
+              저장한 글
             </A.MenuSubtitleInActive>
-            {/* <A.MenuSubtitleActive to="/myreview/save">
-            저장한 글
-          </A.MenuSubtitleActive> */}
           </A.MenuSubtitleContainer>
         </div>
       </A.SideHeader>
@@ -92,23 +87,35 @@ function Breeder() {
           </A.SelectContainer>
 
           <A.CardsContainer>
-            {/* {bookmarkedBreeder.map((breeder, index) => ( //필터링된 카드 나열
-            <BreederCard
-              key={index}
-              photo={breeder.photo}
-              location={breeder.location}
-              name={breeder.name}
-              breederExperience={breeder.breederExperience}
-              numberOfCertifications={breeder.numberOfCertifications}
-              waitingDogs={breeder.waitingDogs}
-              waitlistCount={breeder.waitlistCount}
-              rating={breeder.rating}
-              reviewCount={breeder.reviewCount}
-              isBookmarked={breeder.isBookmarked}
-              setIsBookmarked= () => {} 
+            <div className="dogCard">
+              {전체BreederCard.map(
+                (
+                  breeder,
+                  index, // 필터링된 카드 나열
+                ) => (
+                  <BreederCard
+                    key={index}
+                    photo={breeder.photo}
+                    location={breeder.location}
+                    name={breeder.name}
+                    breederExperience={breeder.breederExperience}
+                    numberOfCertifications={breeder.numberOfCertifications}
+                    waitingDogs={breeder.waitingDogs}
+                    waitlistCount={breeder.waitlistCount}
+                    rating={breeder.rating}
+                    reviewCount={breeder.reviewCount}
+                    isBookmarked={breeder.isBookmarked}
+                    setIsBookmarked={() => {}}
+                  />
+                ),
+              )}
+            </div>
+            <Pagination
+              totalItems={전체BreederCard.length}
+              itemsPerPage={20}
+              currentPage={currentPage}
+              setCurrentPage={setCurrentPage}
             />
-          ))} */}
-            <BreederCard />
           </A.CardsContainer>
         </A.BottomContainer>
       </A.MainContainer>
