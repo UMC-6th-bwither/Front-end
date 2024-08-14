1e392a2cc9da7b33a0d05db97f2e6374dbb708db
jisooooooooooo <dlawltn0708@naver.com>
2024-08-15 03:30:34 +0900

    💄 Design: 분양대기동물 페이지 디자인 수정

--- src/pages/WaitingAnimalDetail/WaitingAnimalDetail.style.js
+++ src/pages/WaitingAnimalDetail/WaitingAnimalDetail.style.js
@@ -10,6 +10,41 @@ export const Container = styled.div`
   padding: 64px 0 291px 0;
 `;
 
+export const ModalOverlay = styled.div`
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background: rgba(0, 0, 0, 0.5);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  z-index: 1000;
+`;
+
+export const ModalContent = styled.div`
+  background: #fff;
+  padding: 50px;
+  border-radius: 10px;
+  max-width: 600px;
+  height: 500px;
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  position: relative;
+`;
+
+export const CloseButton = styled.button`
+  position: absolute;
+  top: 16px;
+  right: 16px;
+  background: none;
+  border: none;
+  cursor: pointer;
+`;
+
 export const Title = styled.div`
   font-size: 24px;
   font-weight: 700;
