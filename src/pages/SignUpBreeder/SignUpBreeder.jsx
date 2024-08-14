7a5601a96f3fab051929651e8598fa0e2472e9da
kayubin <kayubin27@gmail.com>
2024-08-15 01:12:08 +0900

    ✨ 브리더 회원가입 생성

---
+++ src/pages/SignUpBreeder/SignUpBreeder.jsx
@@ -0,0 +1,227 @@
+/* eslint-disable react/jsx-props-no-spreading */
+import { useForm } from 'react-hook-form';
+import { useState } from 'react';
+import * as B from './SignUpBreeder.style';
+import progressBar25 from '../../../public/icons/signUp/progress-bar-25.svg';
+import pwShow from '../../../public/icons/signUp/password-show.svg';
+import Terms from '../../components/SignUpTerms/SignUpTerms';
+import Button from '../../components/SignUpButton/Button';
+import TextButton from '../../components/SignUpButton/TextButton';
+import failX from '../../../public/icons/signUp/fail_x.svg';
+
+export default function SignUpBreeder() {
+  const [pwdVisible, setPwdVisible] = useState({
+    password: false,
+    passwordCheck: false,
+  });
+
+  // 비밀번호 숨기기 & 보여주기
+  const toggleVisibility = (field) => {
+    setPwdVisible((prevState) => ({
+      ...prevState,
+      [field]: !prevState[field],
+    }));
+  };
+
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm({ mode: 'all' });
+
+  const onSubmit = (data) => {
+    console.log('Form submitted.', data);
+  };
+
+  return (
+    <B.Background>
+      <B.WelcomeMsg>
+        환영해요! <br /> 당신의 소중한 반려동물을 위한 첫 걸음을 시작해보세요.
+      </B.WelcomeMsg>
+      <B.Container onSubmit={handleSubmit(onSubmit)}>
+        <B.ProgressBar src={progressBar25} />
+        <B.Text>브위더 계정을 만들어 보아요</B.Text>
+        <B.InputArea>
+          <B.InputWrapper>
+            <B.InputTitle>이름</B.InputTitle>
+            <B.InputBox
+              type="text"
+              placeholder="실명을 입력해주세요"
+              {...register('name', {
+                required: '이름을 입력해주세요',
+                minLength: { value: 2, message: '2자 이상 입력해주세요' },
+              })}
+            />
+            {errors.name && (
+              <B.Error>
+                <B.FailX>
+                  <img src={failX} alt="X" />
+                </B.FailX>
+                <div>{errors.name.message}</div>
+              </B.Error>
+            )}
+          </B.InputWrapper>
+          <B.InputWrapper>
+            <B.InputTitle>전화번호</B.InputTitle>
+            <B.InputBox
+              type="tel"
+              placeholder="010-1234-5678 형태로 입력해주세요"
+              {...register('phone', {
+                required: '전화번호를 입력해주세요',
+                pattern: {
+                  value: /^\d{3}-\d{4}-\d{4}$/,
+                  message: '전화번호를 형식에 맞게 입력해주세요',
+                },
+              })}
+            />
+            {errors.phone && (
+              <B.Error>
+                <B.FailX>
+                  <img src={failX} alt="X" />
+                </B.FailX>
+                <div>{errors.phone.message}</div>
+              </B.Error>
+            )}
+          </B.InputWrapper>
+          <B.InputWrapper>
+            <B.InputTitle>이메일</B.InputTitle>
+            <div className="InputButtonWrapper">
+              <B.InputBox
+                type="email"
+                placeholder="example@email.com"
+                required
+                {...register('email', {
+                  required: '이메일을 입력해주세요',
+                  pattern: {
+                    value: /^[a-zA-Z0-9+-_.]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/,
+                    message: '올바른 이메일 형식이 아닙니다.',
+                  },
+                })}
+              />
+              <B.CodeRequestButton>인증번호</B.CodeRequestButton>
+            </div>
+            {errors.email && (
+              <B.Error>
+                <B.FailX>
+                  <img src={failX} alt="X" />
+                </B.FailX>
+                <div>{errors.email.message}</div>
+              </B.Error>
+            )}
+          </B.InputWrapper>
+          <B.InputWrapper>
+            <B.InputTitle>인증번호 입력</B.InputTitle>
+            <div className="InputButtonWrapper">
+              <B.InputBox
+                type="tel"
+                placeholder="문자로 전송된 인증번호 6자리"
+                {...register('phone', {
+                  required: '전화번호를 입력해주세요',
+                  pattern: {
+                    value: /^\d{3}-\d{4}-\d{4}$/,
+                    message: '전화번호를 형식에 맞게 입력해주세요',
+                  },
+                })}
+              />
+              <B.CodeRequestButton>인증하기</B.CodeRequestButton>
+            </div>
+            {errors.phone && (
+              <B.Error>
+                <B.FailX>
+                  <img src={failX} alt="X" />
+                </B.FailX>
+                <div>{errors.phone.message}</div>
+              </B.Error>
+            )}
+          </B.InputWrapper>
+          <B.InputWrapper>
+            <B.InputTitle>아이디</B.InputTitle>
+            <div className="InputButtonWrapper">
+              <B.InputBox
+                type="tel"
+                placeholder="아이디를 입력해주세요"
+                {...register('username', {
+                  required: '아이디를 입력해주세요',
+                })}
+              />
+              <B.CodeRequestButton>중복확인</B.CodeRequestButton>
+            </div>
+            {errors.phone && (
+              <B.Error>
+                <B.FailX>
+                  <img src={failX} alt="X" />
+                </B.FailX>
+                <div>{errors.name.message}</div>
+              </B.Error>
+            )}
+          </B.InputWrapper>
+          <B.InputWrapper>
+            <B.InputTitle>비밀번호</B.InputTitle>
+            <div className="InputButtonWrapper">
+              <B.InputBox
+                type={pwdVisible.password ? 'text' : 'password'}
+                placeholder="영문, 숫자, 특수문자 포함 8자 이상 적어주세요"
+                {...register('password', {
+                  required: '비밀번호를 입력해주세요',
+                  pattern: {
+                    value:
+                      /^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/,
+                    message: '비밀번호를 형식에 맞게 입력해주세요',
+                  },
+                })}
+              />
+              <B.PwShowIcon
+                src={pwShow}
+                onClick={() => toggleVisibility('password')}
+              />
+            </div>
+            {errors.password && (
+              <B.Error>
+                <B.FailX>
+                  <img src={failX} alt="X" />
+                </B.FailX>
+                <div>{errors.password.message}</div>
+              </B.Error>
+            )}
+          </B.InputWrapper>
+          <B.InputWrapper>
+            <B.InputTitle>비밀번호 확인</B.InputTitle>
+            <div className="InputButtonWrapper">
+              <B.InputBox
+                type={pwdVisible.passwordCheck ? 'text' : 'password'}
+                placeholder="영문, 숫자, 특수문자 포함 8자 이상 적어주세요"
+                {...register('passwordCheck', {
+                  required: '비밀번호를 입력해주세요',
+                  validate: (value) =>
+                    value === watch('password') ||
+                    '비밀번호를 형식에 맞게 입력해주세요.',
+                })}
+              />
+              <B.PwShowIcon
+                src={pwShow}
+                onClick={() => toggleVisibility('passwordCheck')}
+              />
+            </div>
+            {errors.passwordCheck && (
+              <B.Error>
+                <B.FailX>
+                  <img src={failX} alt="X" />
+                </B.FailX>
+                <div>{errors.passwordCheck.message}</div>
+              </B.Error>
+            )}
+          </B.InputWrapper>
+        </B.InputArea>
+        <Terms />
+        <B.BtnWrapper1>
+          <Button type="submit" text="회원가입" path="SignUpBreederChoice" />
+          <TextButton
+            path="BwitherSignUp1"
+            text="일반 회원으로 가입하시나요?"
+          />
+        </B.BtnWrapper1>
+      </B.Container>
+    </B.Background>
+  );
+}
---
+++ src/pages/SignUpBreeder/SignUpBreederInfo2.jsx
@@ -0,0 +1,133 @@
+import { useState, forwardRef } from 'react';
+import PropTypes from 'prop-types';
+import * as B from './SignUpBreeder.style';
+import progressBar90 from '../../../public/icons/signUp/progress-bar-90.svg';
+import upload from '../../../public/icons/signUp/upload_plus.svg';
+import plus from '../../../public/icons/signUp/plusBtn_plus.svg';
+import calendar from '../../../public/icons/signUp/calendar.svg';
+import Button from '../../components/SignUpButton/Button';
+import unchecked from '../../../public/img/uncheckedCheck.svg';
+
+const CustomInput = forwardRef(({ value, onClick, placeholder }, ref) => (
+  <B.CustomInput
+    ref={ref}
+    onClick={onClick}
+    placeholder={placeholder}
+    value={value}
+    readOnly
+  />
+));
+CustomInput.displayName = 'CustomInput';
+
+CustomInput.propTypes = {
+  value: PropTypes.string,
+  onClick: PropTypes.func,
+  placeholder: PropTypes.string.isRequired,
+};
+
+CustomInput.defaultProps = {
+  value: '',
+  onClick: () => {},
+};
+
+export default function SignUpBreederInfo2() {
+  const [startDate, setStartDate] = useState(null);
+
+  return (
+    <B.Background>
+      <B.WelcomeMsg>
+        환영해요! <br /> 브위더와 함께 올바른 반려동물 분양문화를 이끌어가요
+      </B.WelcomeMsg>
+      <B.Container>
+        <B.ProgressBar src={progressBar90} />
+        <B.Text>거의 다 왔어요! 더욱 자세히 알고싶어요</B.Text>
+        <B.InputArea>
+          <B.InputWrapper2>
+            <B.InputTitle>
+              운영하고 있는 켄넬/캐터리 주소<B.Star>*</B.Star>
+            </B.InputTitle>
+            <B.InputBoxWrapper>
+              <B.InputBox2 placeholder="우편번호 입력" />
+              <B.PostCodeBtn>우편번호 찾기</B.PostCodeBtn>
+            </B.InputBoxWrapper>
+            <B.InputBox2 placeholder="주소 입력" />
+            <B.InputBox2 placeholder="상세 주소" />
+          </B.InputWrapper2>
+          <B.InputWrapper2>
+            <B.InputTitle>
+              캔넬/캐터리 사진<B.Star>*</B.Star>
+            </B.InputTitle>
+            <B.UploadFileWrapper>
+              <B.FileUpload>
+                <img src={upload} alt="upload" />
+                <div className="text">파일 첨부</div>
+              </B.FileUpload>
+              <B.FileUpload />
+              <B.FileUpload />
+              <B.FileUpload />
+              <B.FileUpload />
+              <B.FileUpload />
+            </B.UploadFileWrapper>
+          </B.InputWrapper2>
+          <B.InputWrapper2>
+            <B.InputTitle>이용 중인 동물병원</B.InputTitle>
+            <B.InputBox2 placeholder="병원명을 입력해주세요" />
+          </B.InputWrapper2>
+          <B.InputWrapper3>
+            <B.InputTitle>자격증</B.InputTitle>
+            <B.InputBoxWrapper>
+              <B.InputBox2 placeholder="자격증명" />
+              <B.PostCodeBtn>파일 첨부</B.PostCodeBtn>
+            </B.InputBoxWrapper>
+            <B.PlusBtn className="btn">
+              <img src={plus} alt="plus_img" />
+              추가하기
+            </B.PlusBtn>
+          </B.InputWrapper3>
+          <B.InputWrapper3>
+            <B.InputTitle>
+              번식 경력<B.Star>*</B.Star>
+            </B.InputTitle>
+            <B.InputBoxWrapper>
+              <B.InputBox3 placeholder="상호명" />
+              <B.DateInputWrapper>
+                <B.CustomDatePicker
+                  selected={startDate}
+                  onChange={(date) => setStartDate(date)}
+                  dateFormat="yyyy/MM"
+                  showMonthYearPicker
+                  customInput={<CustomInput placeholder="입사연월" />}
+                  placeholderText="입사연월"
+                />
+                <B.CalendarIcon src={calendar} alt="calendar" />
+              </B.DateInputWrapper>
+              <B.Hyphen />
+              <B.DateInputWrapper>
+                <B.CustomDatePicker
+                  selected={startDate}
+                  onChange={(date) => setStartDate(date)}
+                  dateFormat="yyyy/MM"
+                  showMonthYearPicker
+                  customInput={<CustomInput placeholder="퇴사연월" />}
+                  placeholderText="퇴사연월"
+                />
+                <B.CalendarIcon src={calendar} alt="calendar" />
+              </B.DateInputWrapper>
+            </B.InputBoxWrapper>
+            <B.CheckWrapper>
+              <img src={unchecked} alt="unchecked_check" /> 재직중
+            </B.CheckWrapper>
+            <B.PlusBtn className="btn">
+              <img src={plus} alt="plus_img" />
+              추가하기
+            </B.PlusBtn>
+          </B.InputWrapper3>
+        </B.InputArea>
+        <B.BtnWrapper2>
+          <Button white text="이전" path="SignUpBreederInfo" />
+          <Button text="다음" path="" />
+        </B.BtnWrapper2>
+      </B.Container>
+    </B.Background>
+  );
+}
