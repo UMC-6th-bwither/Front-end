1d569b391c7faa5014473ac569e6be614fbfea42
kwdahun <kwdahun@naver.com>
2024-08-19 22:55:39 +0900

    ✨ 로그인 hook useAuth 생성

--- src/utils/extractImgSrcFromBlocks.js
+++ src/utils/extractImgSrcFromBlocks.js
@@ -1,12 +1,10 @@
 export default function extractFirstImageUrl(blocks) {
   const imageBlock = blocks.find((block) => {
-    const parsedBlock = JSON.parse(block.block); // JSON 문자열을 객체로 변환
-    return parsedBlock.type === 'image';
+    return block.type === 'image'; // 이미지 타입의 블록을 찾음
   });
 
   if (imageBlock) {
-    const parsedBlock = JSON.parse(imageBlock.block);
-    return parsedBlock.data.file.url; // 첫 번째 이미지의 URL 반환
+    return imageBlock.data.file.url; // 첫 번째 이미지의 URL 반환
   }
 
   return null; // 이미지 블록이 없을 경우 null 반환
