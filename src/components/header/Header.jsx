90513fd6a4faca8ef37e346862e948f067804197
techncherry <technarchitect@gmail.com>
2024-08-22 20:13:12 +0900

    ✨ Feat: Header role에 따라 바뀌게 구현

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -12,49 +12,76 @@ export default function Header() {
   if (isLoggedIn) {
     if (role === 'MEMBER') {
       profileLink = (
-        <H.TextWrapper>
-          <Link to="MypageGeneral">
-            <H.Profile src={profile} alt="profile" />
-          </Link>
-        </H.TextWrapper>
+        <H.Container>
+          <H.SubContainer>
+            <H.Left>
+              <Link to="/">
+                <H.Logo src={logo} alt="logo" />
+              </Link>
+              <H.TextWrapper>
+                <H.NavText to="animal-list">분양 대기 동물</H.NavText>
+                <H.NavText to="breeder-list">브리더 찾기</H.NavText>
+                <H.NavText to="adoptionsystem">분양 시스템</H.NavText>
+                <H.NavText to="community">커뮤니티</H.NavText>
+              </H.TextWrapper>
+            </H.Left>
+            <H.TextWrapper>
+              <Link to="MypageGeneral">
+                <H.Profile src={profile} alt="profile" />
+              </Link>
+            </H.TextWrapper>
+          </H.SubContainer>
+        </H.Container>
       );
     } else {
       profileLink = (
-        <H.TextWrapper>
-          <Link to="MypageBreeder">
-            <H.Profile src={profile} alt="profile" />
-          </Link>
-        </H.TextWrapper>
+        <H.Container>
+          <H.SubContainer>
+            <H.Left>
+              <Link to="/">
+                <H.Logo src={logo} alt="logo" />
+              </Link>
+              <H.TextWrapper>
+                <H.NavText to="BreederAnimalList">관리 중인 동물</H.NavText>
+                <H.NavText to="adoptionsystem">분양 시스템</H.NavText>
+                <H.NavText to="community">커뮤니티</H.NavText>
+              </H.TextWrapper>
+            </H.Left>
+            <H.TextWrapper>
+              <Link to="MypageBreeder">
+                <H.Profile src={profile} alt="profile" />
+              </Link>
+            </H.TextWrapper>
+          </H.SubContainer>
+        </H.Container>
       );
     }
   } else {
     profileLink = (
-      <H.Right>
-        <H.TextWrapper>
-          <H.AuthText to="login">로그인</H.AuthText>
-          <H.AuthText to="signup/1">회원가입</H.AuthText>
-        </H.TextWrapper>
-        <H.Button to="breeder-signup/1">브리더로 가입</H.Button>
-      </H.Right>
+      <H.Container>
+        <H.SubContainer>
+          <H.Left>
+            <Link to="/">
+              <H.Logo src={logo} alt="logo" />
+            </Link>
+            <H.TextWrapper>
+              <H.NavText to="animal-list">분양 대기 동물</H.NavText>
+              <H.NavText to="breeder-list">브리더 찾기</H.NavText>
+              <H.NavText to="adoptionsystem">분양 시스템</H.NavText>
+              <H.NavText to="community">커뮤니티</H.NavText>
+            </H.TextWrapper>
+          </H.Left>
+          <H.Right>
+            <H.TextWrapper>
+              <H.AuthText to="login">로그인</H.AuthText>
+              <H.AuthText to="signup/1">회원가입</H.AuthText>
+            </H.TextWrapper>
+            <H.Button to="breeder-signup/1">브리더로 가입</H.Button>
+          </H.Right>
+        </H.SubContainer>
+      </H.Container>
     );
   }
 
-  return (
-    <H.Container>
-      <H.SubContainer>
-        <H.Left>
-          <Link to="/">
-            <H.Logo src={logo} alt="logo" />
-          </Link>
-          <H.TextWrapper>
-            <H.NavText to="animal-list">분양 대기 동물</H.NavText>
-            <H.NavText to="breeder-list">브리더 찾기</H.NavText>
-            <H.NavText to="adoptionsystem">분양 시스템</H.NavText>
-            <H.NavText to="community">커뮤니티</H.NavText>
-          </H.TextWrapper>
-        </H.Left>
-        {profileLink}
-      </H.SubContainer>
-    </H.Container>
-  );
+  return <>{profileLink}</>;
 }
