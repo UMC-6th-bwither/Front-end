4f350b50366e7b61047bd6e80340db0cc477fb29
kwdahun <kwdahun@naver.com>
2024-08-30 17:24:57 +0900

    ✨ 후기 삭제 버튼 추가

--- src/components/BreederReviewAnimalCard/BreederReviewAnimalCard.jsx
+++ src/components/BreederReviewAnimalCard/BreederReviewAnimalCard.jsx
@@ -1,5 +1,8 @@
 /* eslint-disable react/prop-types */
+import { useState } from 'react';
 import * as S from './BreederReviewAnimalCard.style';
+import useAuth from '../../hooks/useAuth';
+import { useNavigate } from 'react-router-dom';
 
 export default function BreederReviewAnimalCard({
   postId,
@@ -9,6 +12,27 @@ export default function BreederReviewAnimalCard({
   star,
   context,
 }) {
+  const [modalOpened, setModalOpened] = useState(false);
+  const { token } = useAuth();
+  const navigate = useNavigate();
+
+  const handleModalClick = async (e) => {
+    e.stopPropagation();
+
+    const apiUrl = import.meta.env.VITE_API_URL;
+    const endPoint = `${apiUrl}/post/${postId}`;
+    await fetch(endPoint, {
+      method: 'DELETE',
+      headers: {
+        Accept: '*/*',
+        Authorization: `Bearer ${token}`,
+      },
+    });
+
+    alert('후기가 삭제되었습니다.');
+    navigate(0);
+  };
+
   return (
     <S.Container onClick={onClick}>
       <S.Square>
@@ -37,22 +61,34 @@ export default function BreederReviewAnimalCard({
             </S.StarFlex>
           </S.FlexGrow>
 
-          <S.IconContainer>
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="4"
-              height="18"
-              viewBox="0 0 4 18"
-              fill="none"
+          <S.IconSectionWrapper>
+            <S.IconContainer
+              onClick={(e) => {
+                e.stopPropagation();
+                setModalOpened(!modalOpened);
+              }}
             >
-              <path
-                fillRule="evenodd"
-                clipRule="evenodd"
-                d="M4 2C4 3.10457 3.10457 4 2 4C0.895431 4 0 3.10457 0 2C0 0.895431 0.895431 0 2 0C3.10457 0 4 0.895431 4 2ZM4 9C4 10.1046 3.10457 11 2 11C0.895431 11 0 10.1046 0 9C0 7.89543 0.895431 7 2 7C3.10457 7 4 7.89543 4 9ZM2 18C3.10457 18 4 17.1046 4 16C4 14.8954 3.10457 14 2 14C0.895431 14 0 14.8954 0 16C0 17.1046 0.895431 18 2 18Z"
-                fill="#C5C5C5"
-              />
-            </svg>
-          </S.IconContainer>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="4"
+                height="18"
+                viewBox="0 0 4 18"
+                fill="none"
+              >
+                <path
+                  fillRule="evenodd"
+                  clipRule="evenodd"
+                  d="M4 2C4 3.10457 3.10457 4 2 4C0.895431 4 0 3.10457 0 2C0 0.895431 0.895431 0 2 0C3.10457 0 4 0.895431 4 2ZM4 9C4 10.1046 3.10457 11 2 11C0.895431 11 0 10.1046 0 9C0 7.89543 0.895431 7 2 7C3.10457 7 4 7.89543 4 9ZM2 18C3.10457 18 4 17.1046 4 16C4 14.8954 3.10457 14 2 14C0.895431 14 0 14.8954 0 16C0 17.1046 0.895431 18 2 18Z"
+                  fill="#C5C5C5"
+                />
+              </svg>
+            </S.IconContainer>
+            {modalOpened && (
+              <S.ModalContainer onClick={handleModalClick}>
+                삭제
+              </S.ModalContainer>
+            )}
+          </S.IconSectionWrapper>
         </S.Flex>
         <S.Context>{context}</S.Context>
       </S.ContentContainer>
